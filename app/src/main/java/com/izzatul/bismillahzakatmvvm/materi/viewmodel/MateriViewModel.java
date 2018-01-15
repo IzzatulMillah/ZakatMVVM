4063368ff2acebfed703dec8db7fc821a04aa36e
IzzatulMillah <izzatulmillah27@gmailcom>
2018-01-15 13:56:52 +0700

    coba kedua

---
+++ app/src/main/java/com/izzatul/bismillahzakatmvvm/materi/viewmodel/MateriViewModel.java
@@ -0,0 +1,10 @@
+package com.izzatul.bismillahzakatmvvm.materi.viewmodel;
+
+import android.databinding.BaseObservable;
+
+/**
+ * Created by Izzatul on 1/15/2018.
+ */
+
+public class MateriViewModel extends BaseObservable{
+}
