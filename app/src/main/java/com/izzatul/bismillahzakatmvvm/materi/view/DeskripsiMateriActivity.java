3f9eb1595315cfccaf652b309af3d6b4a5d18cb8
IzzatulMillah <izzatulmillah27@gmailcom>
2018-01-16 14:13:33 +0700

    update 1.0

--- app/src/main/java/com/izzatul/bismillahzakatmvvm/materi/view/DeskripsiMateriActivity.java
+++ app/src/main/java/com/izzatul/bismillahzakatmvvm/materi/view/DeskripsiMateriActivity.java
@@ -2,14 +2,52 @@ package com.izzatul.bismillahzakatmvvm.materi.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.izzatul.bismillahzakatmvvm.R;
 
 public class DeskripsiMateriActivity extends AppCompatActivity {
 
+    private TextView tvJudulMateri, tvDeskripsi;
+    private int bundleMateri;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.deskripsi_materi_activity);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(R.string.materi);
+        toolbar.setNavigationIcon(android.support.v7.appcompat.R.drawable.abc_ic_ab_back_material);
+
+        tvJudulMateri = findViewById(R.id.tv_judul_materi);
+        tvDeskripsi = findViewById(R.id.tv_deskripsi_materi);
+
+        bundleMateri = getIntent().getExtras().getInt("kategori");
+        if (bundleMateri == 1){
+            Toast.makeText(this, "Zakat Fitrah", Toast.LENGTH_SHORT).show();
+        } else if (bundleMateri == 2){
+            Toast.makeText(this, "Zakat Emas", Toast.LENGTH_SHORT).show();
+        } else if (bundleMateri == 3){
+            Toast.makeText(this, "Zakat Perak", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    // tombol click back ke home atau activity sebelumnya
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int idItem = item.getItemId();
+        switch (idItem) {
+            case android.R.id.home :
+                finish();
+                break;
+            default:
+                Toast.makeText(this, "what are you pushing?", Toast.LENGTH_SHORT).show();
+        }
+        return true;
     }
 }
