e3907cfdc91e88db9cedff91e0050ab1894c4ee8
IzzatulMillah <izzatulmillah27@gmailcom>
2018-01-20 16:17:00 +0700

    update 1.5

---
+++ app/src/main/java/com/izzatul/bismillahzakatmvvm/kalkulator/HitungZakatHewanTernakActivity.java
@@ -0,0 +1,13 @@
+package com.izzatul.bismillahzakatmvvm.kalkulator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HitungZakatHewanTernakActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hitung_zakat_hewan_ternak);
+    }
+}
---
+++ app/src/main/java/com/izzatul/bismillahzakatmvvm/latihan/model/Pertanyaan.java
@@ -0,0 +1,8 @@
+package com.izzatul.bismillahzakatmvvm.latihan.model;
+
+/**
+ * Created by Izzatul on 1/20/2018.
+ */
+
+public class Pertanyaan {
+}
